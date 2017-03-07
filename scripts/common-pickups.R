bcce7f7dc3b5737ce28dff796728af4fddb94d3f
Josh Weinflash <Josh@Joshs-MacBook-Pro.local>
2017-03-07 08:00:03 -0500

    making changes to pickups plot.. still not quite working

--- scripts/common-pickups.R
+++ scripts/common-pickups.R
@@ -13,13 +13,13 @@ library("DBI")
 con = dbConnect(RSQLite::SQLite(), dbname = "../data/taxi.db")
 
 query = paste('SELECT period, area_no, COUNT(*) AS count
-               FROM (
-                SELECT "Pickup Community Area" AS area_no, 
-                        PRINTF("%s-%s", SUBSTR("Trip Start Timestamp", 7, 4),
-                                        SUBSTR("Trip Start Timestamp", 1, 2)) AS period
-                FROM taxi
-                LIMIT 100
-               )
+              FROM (
+              SELECT "Pickup Community Area" AS area_no, 
+              PRINTF("%s-%s", SUBSTR("Trip Start Timestamp", 7, 4),
+              SUBSTR("Trip Start Timestamp", 1, 2)) AS period
+              FROM taxi
+              LIMIT 100
+              )
               GROUP BY period, area_no'
 )
 
@@ -29,34 +29,46 @@ df_pick = dbGetQuery(con, query)
 df_pick$period = lubridate::quarter(as.Date(sprintf("%s-01", df_pick$period)), 
                                     with_year = TRUE)
 
+# sum of counts belonging to the same (quarter, community area)
+df_pick = plyr::ddply(df_pick, c("period", "area_no"), my_sum)
+
 # get-community-areas -----------------------------------------------------
 sp_comm = readOGR("../data/", "community-area")
 
 # note: city of chicago's data uses coordinates under the WGS84 standard. 
-# This is good, since it's also what google maps uses
+# stamen uses this too, so we're all set
 print(proj4string(sp_comm))
 
 # convert to dataframe form so it works with ggplot
-df_comm = extract_community_area_data(sp_comm)
+# df_comm = extract_community_area_data(sp_comm)
+df_comm = fortify(sp_comm)
 
 # merge-count-and-lon-lat-data --------------------------------------------
-df_pick = merge(df_pick, df_comm, by = "area_no", all.x = TRUE)
+df_pick = merge(df_pick, df_comm, by.x = "area_no", by.y = "id")
+
+df_pick = df_pick[order(df_pick$order), ]
 
 # build-plot --------------------------------------------------------------
 
 # first get the "bounding box" that contains Chicago (needed for stamen maps)
-gm_chi = get_googlemap("Chicago, Illinois")
-bbox = bb2bbox(attr(gm_chi, "bb"))
+ggm_chi = get_googlemap("Chicago, Illinois")
+bbox = bb2bbox(attr(ggm_chi, "bb"))
 
 # pull the stamenmap with these bounds
-gm_chi = get_stamenmap(bbox, zoom = 10, maptype = "toner-lite")
+ggm_chi = get_stamenmap(bbox, zoom = 10, maptype = "toner-lite")
 
-# TODO: show boundaries based on comm. areas; fill using counts
-gp_chi = ggmap(gm_chi, base_layer = ggplot(df_comm, aes_string("lon", "lat")), 
-               maprange = TRUE, extent = "device")
+# construct plot
+ggp_chi = ggmap(ggm_chi, base_layer = ggplot(df_pick, aes_string("long", "lat")), 
+                maprange = TRUE, extent = "device")
 
-gp_chi = gp_chi + geom_path(aes_string(group = "area_no"))
+ggp_chi = ggp_chi + geom_polygon(aes_string(fill = "count"))
+ggp_chi = ggp_chi + facet_wrap("period")
+
+ggp_chi = ggp_chi + scale_fill_gradient(low = "#f7fcf5", high = "#00441b",
+                                        guide = "legend")
+
+ggp_chi = ggp_chi + theme(legend.position = "top")
 
 # save-to-file ------------------------------------------------------------
-ggsave("../plots/pickups.png", plot = gp_chi, height = 11, width = 8.5, 
-       units = "in")
+ggsave("../plots/pickups.png", plot = ggp_chi, height = 11, 
+       width = 8.5,units = "in")
--- scripts/helper.R
+++ scripts/helper.R
@@ -16,6 +16,7 @@ extract_community_area_data = function(spdf) {
 
   # extract the 'data' and 'polygons' slots from the object
   df_comm = spdf@data
+  v_order = spdf@plotOrder
   l_polys = spdf@polygons
   
   # init list to hold output
@@ -30,11 +31,31 @@ extract_community_area_data = function(spdf) {
                                          # to the first element
     l_dfs[[i]] = data.frame("area_no" = rep(as.character(df_comm$area_numbe)[i], nrow(m_cord)),
                             "area_nm" = rep(as.character(df_comm$community)[i], nrow(m_cord)),
+                            "order" = rep(v_order[i], nrow(m_cord)),
                             "lon" = m_cord[, 1],
                             "lat" = m_cord[, 2],
                             stringsAsFactors = FALSE)
   }
   
   return(plyr::rbind.fill(l_dfs))
+}
+
+my_sum = function(df) {
+  # quick function to add up the counts 
+  # belonging to a particular community in a 
+  # particular quarter (the db query returns 
+  # counts by month)
+  #
+  # Args:
+  #   df: data.frame
+  #
+  # Returns: data.frame
+
+  df_out = data.frame("period" = df$period[1],
+                      "area_no" = df$area_no[1],
+                      "count" = sum(df$count),
+                      stringsAsFactors = FALSE)
+  
+  return(df_out)
 }
 
